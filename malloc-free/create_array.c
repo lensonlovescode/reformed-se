30d78f77f8aef0ad142db96cb66a29eb9db8ceda
lensonlovescode <lensonisinparis@gmail.com>
2024-01-10 20:14:28 +0300

    till tommorrow

---
+++ malloc-free/create_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (size == NULL)
+		return (NULL);
+
+}
