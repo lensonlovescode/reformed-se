d636423871bfda3d36bd99b38652a59fd5cc6dda
lensonlovescode <lensonisinparis@gmail.com>
2024-01-16 19:31:47 +0300

    __FILE__ macro

--- perprocessor/2-main.c
+++ perprocessor/2-main.c
@@ -1,12 +1,15 @@
-#include <stdlib>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- *
- *
- *
+ * main - prints out the name of the file from which
+ * the program was compiled
+ * 
+ * Return- returns 0
  *
  *
  */
- int main()
+ int main(void)
  {
-
+	printf("%s\n", __FILE__);
+	return (0);
  }
