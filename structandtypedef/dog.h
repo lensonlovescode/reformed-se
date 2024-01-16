d14ad672ad2df0d800ffed7860f14ca0f9556bce
lensonlovescode <lensonisinparis@gmail.com>
2024-01-16 22:07:21 +0300

    initialize struct dog

--- structandtypedef/dog.h
+++ structandtypedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - dog  struct with members age, name and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ */
 struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif
 
