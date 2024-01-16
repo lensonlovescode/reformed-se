404e770b7638da7c1ed55624ccd59d2a0868958b
lensonlovescode <lensonisinparis@gmail.com>
2024-01-16 19:57:13 +0300

    SUM macro plus betty: return (0); :)

--- perprocessor/2-main.c
+++ perprocessor/2-main.c
@@ -3,13 +3,11 @@
 /**
  * main - prints out the name of the file from which
  * the program was compiled
- * 
- * Return- returns 0
  *
- *
+ * Return: returns 0
  */
- int main(void)
- {
+int main(void)
+{
 	printf("%s\n", __FILE__);
 	return (0);
- }
+}
