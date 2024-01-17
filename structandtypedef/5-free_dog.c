379ba9aa951e314f25ef216fc13f8917cbfa1a8d
lensonlovescode <lensonisinparis@gmail.com>
2024-01-17 11:41:42 +0300

    last task: free dog

---
+++ structandtypedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog - frees dog
+ * @d: this is the struct to be freed
+ *
+ *
+ *
+ *
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
