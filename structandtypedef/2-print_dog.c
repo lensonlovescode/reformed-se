e41a506a8b6ec4ec6e89b5218f5b0cd25f8683ed
lensonlovescode <lensonisinparis@gmail.com>
2024-01-17 09:08:07 +0300

    float cannot be null?

--- structandtypedef/2-print_dog.c
+++ structandtypedef/2-print_dog.c
@@ -11,23 +11,24 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		if (d->name != NULL){
-			printf("Name: %s\n", d->name);
-		}
-		else{
-			printf("Name: (nil)");
-		}
-		if (d->age <= 0){
-			printf("Age: %f\n", d->age);
-		}
-		else{
-			printf("Age: (nil)");
-		}
-		if (d->owner != NULL){
-			printf("Owner: %s\n", d->owner);
-		}
-		else{
-			printf("Owner: (nil)");
-		}
+		exit(98);
+	}
+	if (d->name != NULL){
+		printf("Name: %s\n", d->name);
+	}
+	else{
+		printf("Name: (nil)\n");
+	}
+	if (d->age != 0.1){
+		printf("Age: %f\n", d->age);
+	}
+	else{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner != NULL){
+		printf("Owner: %s\n", d->owner);
+	}
+	else{
+		printf("Owner: (nil)\n");
 	}
 }
