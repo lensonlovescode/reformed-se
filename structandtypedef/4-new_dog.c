d7d939f0e0ef75111f1b2154c2d908f7a9e3cbaf
lensonlovescode <lensonisinparis@gmail.com>
2024-01-17 11:34:03 +0300

    Improved on ERROR handling

--- structandtypedef/4-new_dog.c
+++ structandtypedef/4-new_dog.c
@@ -10,22 +10,72 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggy;
-	char *DOG_name;
-	char *DOG_owner;
-	float DOG_age;
+	int name_len;
+	int owner_len;
 
 	doggy = malloc(sizeof(dog_t));
-
-	DOG_owner = owner;
-	DOG_name = name;
-	DOG_age = age;
 	
-	doggy->name = DOG_name;
-	doggy->owner = DOG_owner;
-	doggy->age = DOG_age;
+	if (name != NULL && owner != NULL)
+	{
+		doggy = malloc(sizeof(dog_t));
+		name_len = _strlen(name) + 1;
+		owner_len = _strlen(owner) + 1;
 
-	if (doggy == NULL){
-		return (NULL);
+		if (doggy == NULL)
+			return (NULL);
+
+		doggy->name = malloc(name_len + 1);
+
+		if (doggy->name == NULL)
+		{
+			free(doggy->name);
+			return (NULL);
+		}
+
+		doggy->owner = malloc(owner_len + 1);
+
+		if (doggy->owner == NULL)
+		{
+			free(doggy->owner);
+			return (NULL);
+		}
+
+		doggy->name = _strcpy(doggy->name, name);
+		doggy->owner = _strcpy(doggy->owner, owner);
+		doggy->age = age;
 	}
 	return (doggy);
 }
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+	}
