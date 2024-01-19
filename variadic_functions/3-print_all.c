20398fd87e0b7e0888b92d4652ac99596da51134
lensonlovescode <lensonisinparis@gmail.com>
2024-01-19 13:11:31 +0300

    switch research pending

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_all - this prints all types of arguments
+ * @separator: this is the separator between strings
+ * @n: this is the number of strings
+ * Return: this will return nothing
+ *
+ *
+ */
+void print_all(const char * const format, ...);
+{
+	char *CURRENT;
+	unsigned int i;
+	va_list args;
+
+	if (separator == NULL)
+	{
+		exit(98);
+	}
+	
+	va_start(args, n);
+
+	while (i != '\0')
+	{
+		
+	}
+	printf("\n");
+}
