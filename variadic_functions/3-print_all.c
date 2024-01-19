25d2c488e646648a328ed7dd52ecbb3e423566f6
lensonlovescode <lensonisinparis@gmail.com>
2024-01-19 22:30:15 +0300

    Last task: print all: switch

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,22 +7,52 @@
  *
  *
  */
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
-	char *CURRENT;
+	unsigned int FORMAT_LEN;
+	float FLOAT_CURR;
+	int INT_CURR;
+	char *STRING_CURR;
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL)
+	va_start(args, format);
+	/**while statement for chacking if formai is null goes here*/
+	FORMAT_LEN = strlen(format);
+	i = 0;
+	while (format[i] != '\0')
 	{
-		exit(98);
+		switch (format[i])
+		{
+			case 'c':
+				INT_CURR = va_arg(args, int);
+				printf("%c", INT_CURR);
+			break;
+			case 'i':
+				INT_CURR = va_arg(args, int);
+				printf("%d", INT_CURR);
+			break;
+			case 'f':
+				FLOAT_CURR = va_arg(args, double);
+				printf("%f", FLOAT_CURR);
+			break;
+			case 's':
+				STRING_CURR = va_arg(args, char*);
+				if (STRING_CURR == NULL)
+				{
+					printf("(nil)");
+				}
+				printf("%s", STRING_CURR);
+			break;
+			default:
+				break;
+		}
+		if (i != FORMAT_LEN - 1 && (format[i + 1] == 'c' ||
+			format[i + 1] == 'i' || format[i + 1] == 'f' || format[i + 1] == 's'))
+		{
+			printf(", ");
+		}
+		i++;
 	}
-	
-	va_start(args, n);
-
-	while (i != '\0')
-	{
-		
-	}
-	printf("\n");
+		printf("\n");
 }
